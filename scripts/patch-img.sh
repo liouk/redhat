93f3fd481c7a4bc96afaf06e0877d706276cc22c
Ilias Rinis <irinis@redhat.com>
2023-07-18 11:57:32 +0300

    patch-img: Reuse patching operand image logic

--- scripts/patch-img.sh
+++ scripts/patch-img.sh
@@ -11,30 +11,8 @@ patch_cluster-policy-controller () {
   local container_idx=0
   local env_var="CLUSTER_POLICY_CONTROLLER_IMAGE"
   local env_var_idx=2
-  local deployment=$(kubectl get deployment -n {openshift-,}$container -o json)
 
-  [[ $(echo "$deployment" | jq ".spec.template.spec.containers[$container_idx].name") == "\"$container\"" ]] || {
-    echo "container at index $container_idx is not '$container'"
-    exit 1
-  }
-
-  [[ $(echo "$deployment" | jq ".spec.template.spec.containers[$container_idx].env[$env_var_idx].name") == "\"$env_var\"" ]] || {
-    echo "env_var at index $env_var_idx is not '$env_var'"
-    exit 1
-  }
-
-  echo "$deployment" \
-    | jq ".spec.template.spec.containers[$container_idx].env[$env_var_idx].value = \"$IMG\"" \
-    | kubectl replace -f -
-
-  echo "New image:"
-  kubectl get deployment -n {openshift-,}$container -o json \
-    | jq "
-    {
-      containers: {
-        name: .spec.template.spec.containers[$container_idx].name,
-        env: .spec.template.spec.containers[$container_idx].env[$env_var_idx]}
-    }"
+  update_operand_image "$container" "$container_idx" "$env_var" "$env_var_idx"
 }
 
 patch_cluster-authentication-operator () {
@@ -58,6 +36,53 @@ patch_oauth-server () {
       containers: {
         name: .spec.template.spec.containers[0].name,
         env: .spec.template.spec.containers[0].env[0]}
+    }"
+}
+
+patch_kube-apiserver-operator () {
+  local container="kube-apiserver-operator"
+  local container_idx=0
+  local env_var="IMAGE"
+  local env_var_idx=0
+
+  update_operand_image "$container" "$container_idx" "$env_var" "$env_var_idx"
+}
+
+update_operand_image () {
+  local container="$1"
+  local container_idx="$2"
+  local env_var="$3"
+  local env_var_idx="$4"
+  local show_only="$5"
+
+  local deployment=$(kubectl get deployment -n {openshift-,}$container -o json)
+
+  [[ $(echo "$deployment" | jq ".spec.template.spec.containers[$container_idx].name") == "\"$container\"" ]] || {
+    echo "container at index $container_idx is not '$container'"
+    exit 1
+  }
+
+  [[ $(echo "$deployment" | jq ".spec.template.spec.containers[$container_idx].env[$env_var_idx].name") == "\"$env_var\"" ]] || {
+    echo "env_var at index $env_var_idx is not '$env_var'"
+    exit 1
+  }
+
+  if [ -z "$show_only" ]; then
+    echo "$deployment" \
+      | jq ".spec.template.spec.containers[$container_idx].env[$env_var_idx].value = \"$IMG\"" \
+      | kubectl replace -f -
+
+    echo "New image:"
+  else
+    echo "Current image:"
+  fi
+
+  kubectl get deployment -n {openshift-,}$container -o json \
+    | jq "
+    {
+      containers: {
+        name: .spec.template.spec.containers[$container_idx].name,
+        env: .spec.template.spec.containers[$container_idx].env[$env_var_idx]}
     }"
 }
 
