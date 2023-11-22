d3adbadc850a362915161091fe289a5867a1814d
Ilias Rinis <irinis@redhat.com>
2023-11-22 16:52:17 +0100

    Unmanage CRDs as well as deployments

--- scripts/cvo-unmanage.sh
+++ scripts/cvo-unmanage.sh
@@ -6,13 +6,13 @@ usage () {
   printf "\n$0 <namespace> <override>\n    Patch the Cluster Version Operator to not manage the specified component\n"
 }
 
-main () {
+deployment () {
   ns="$1"
   name="$2"
   value="${3:-true}"
 
   [[ -z "$ns" ]] && { echo "error: undefined namespace"; usage; exit 1; }
-  [[ -z "$name" ]] && { echo "error: undefined override name"; usage; exit 1; }
+  [[ -z "$name" ]] && { echo "error: undefined deployment override name"; usage; exit 1; }
 
   kubectl patch clusterversion version --type='merge' -p "$(cat <<- EOF
 spec:
@@ -24,6 +24,33 @@ spec:
     unmanaged: $value
 EOF
   )"
+}
+
+crd () {
+  name="$1"
+  value="${2:-true}"
+
+  [[ -z "$name" ]] && { echo "error: undefined CRD override name"; usage; exit 1; }
+
+  kubectl patch clusterversion version --type='merge' -p "$(cat <<- EOF
+spec:
+  overrides:
+  - group: config.openshift.io
+    kind: CustomResourceDefinition
+    name: $name
+    namespace: ""
+    unmanaged: $value
+EOF
+  )"
+}
+
+main () {
+  if [[ "$1" == "crd" ]]; then
+    shift
+    crd "$@"
+  else
+    deployment "$@"
+  fi
 }
 
 main "$@"
