c906365f933cd1137ca751e2287839b8ebef48d6
Ilias Rinis <irinis@redhat.com>
2023-11-28 14:51:09 +0100

    Update deploy script for BYO-OIDC

--- issues/BYO-OIDC/deploy.sh
+++ issues/BYO-OIDC/deploy.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
+red () {
+  echo -e "\e[41m\e[30m\e[1m $1 \e[0m $2"
+}
+
+green () {
+  echo -e "\e[42m\e[30m\e[1m $1 \e[0m $2"
+}
+
+yellow () {
+  echo -e "\e[43m\e[30m\e[1m $1 \e[0m $2"
+}
+
 step () {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[42m\e[30m\e[1m  OK  \e[0m $1"
-  else
-    echo -e "\e[41m\e[30m\e[1m FAIL \e[0m $1"
+  if [ $? -ne 0 ]; then
+    red "FAIL" "$1"
     exit $?
   fi
+
+  green " OK " "$1"
 }
 
-todo () {
-  echo -e "\e[43m\e[30m\e[1m TODO \e[0m $1"
-}
+start_from_step="${1:-0}"
 
-# un-manage the Authentication CRD on the CVO
-kubectl patch clusterversion version --type='merge' -p "$(cat <<- EOF
+if [ $start_from_step -le 1 ]; then
+  # un-manage the Authentication CRD on the CVO
+  kubectl patch clusterversion version --type='merge' -p "$(cat <<- EOF
 spec:
   overrides:
   - group: config.openshift.io
@@ -23,21 +34,46 @@ spec:
     namespace: ""
     unmanaged: true
 EOF
+  )"
+  step "un-manage the Authentication CRD on the CVO\n"
+else
+  yellow "SKIP" "un-manage the Authentication CRD on the CVO\n"
+fi
+
+if [ $start_from_step -le 2 ]; then
+  # deploy new CRD
+  crd_file="$HOME/redhat/repos/stlaz/api/config/v1/0000_10_config-operator_01_authentication.crd-TechPreviewNoUpgrade.yaml"
+  echo "New CRD file: $crd_file"
+  read -p "Enter or new: " user_inp
+  [[ -n "$user_inp" ]] && crd_file="$user_inp"
+  echo "Will apply '$crd_file'"
+  kubectl apply -f "$crd_file"
+  step "deploy new Authentication CRD\n"
+else
+  yellow "SKIP" "deploy new Authentication CRD\n"
+fi
+
+if [ $start_from_step -le 3 ]; then
+  patch="$(cat <<- EOF
+spec:
+  type: OIDC
+  webhookTokenAuthenticator:
+  oidcProviders:
+  - name: myoidc
+    issuer:
+      issuerURL: https://meh.tld
+      audiences: ['openshift-aud']
+EOF
 )"
-step "un-manage the Authentication CRD on the CVO"
+  echo -e "Will patch Authentication with:\n$patch\n"
+  kubectl patch authentication cluster --type='merge' -p "$patch"
+  step "update Authentication with type OIDC and OIDCProvider\n"
+else
+  yellow "SKIP" "update Authentication with type OIDC and OIDCProvider\n"
+fi
 
-# deploy new CRD
-crd_file="$HOME/redhat/repos/stlaz/api/config/v1/0000_10_config-operator_01_authentication.crd-CustomNoUpgrade.yaml"
-echo "New CRD file: $crd_file"
-read -p "OK or new: " user_inp
-[[ -n "$user_inp" ]] && crd_file="$user_inp"
-kubectl apply -f "$crd_file"
-step "deploy new Authentication CRD"
+yellow "TODO" "create fake OIDC client\n"
 
-todo "deploy an OIDC provider (keycloak)"
+yellow "TODO" "update Authentication with fake OIDC client\n"
 
-todo "create OIDC client"
-
-todo "update Authentication with OIDC Provider"
-
-todo "deploy console-operator"
+yellow "TODO" "deploy console-operator\n"
