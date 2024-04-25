c421f7129b835f8fa59540a3376a35cb495fdb1f
Ilias Rinis <irinis@redhat.com>
2024-04-25 15:36:59 +0200

    sort by current_flakes

--- issues/AUTH-482_scc-pinning/process-sippy.sh
+++ issues/AUTH-482_scc-pinning/process-sippy.sh
@@ -5,6 +5,6 @@ sippy_export_file="$1"
 echo "| # | Component | Namespace | # Runs | # Successes | # Flakes | # Failures |"
 echo "| --- | --- | --- | --- | --- | --- | --- |"
 cat "$sippy_export_file" | \
-  jq --raw-output '.[] | "| \(.jira_component) | \(.name) | \(.current_runs) | \(.current_successes) | \(.current_flakes) | \(.current_failures)"' | \
+  jq --raw-output 'sort_by(.current_flakes) | .[] | "| \(.jira_component) | \(.name) | \(.current_runs) | \(.current_successes) | \(.current_flakes) | \(.current_failures)"' | \
   awk '{print "| " NR " " $0}' | \
   sed -e "s/\[sig-auth\] all workloads in ns\/\([^[:space:]]*\) must set the 'openshift.io\/required-scc' annotation/\1/"
