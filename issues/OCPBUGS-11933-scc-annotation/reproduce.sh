260fa46ff46aff8efd36347040e6a7b703fb276f
Ilias Rinis <irinis@redhat.com>
2024-05-28 15:37:50 +0200

    Parametrize ns name

--- issues/OCPBUGS-11933-scc-annotation/reproduce.sh
+++ issues/OCPBUGS-11933-scc-annotation/reproduce.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 pod_name="${1:-badpod}"
+ns="irinis-test"
 
 # SCCs are disabled in the 'default' namespace, therefore we need
 # to create a new one to reproduce the issue
-oc delete namespace test
-oc create namespace test
+oc delete namespace $ns
+oc create namespace $ns
 
 # Warning: would violate PodSecurity "restricted:v1.24": allowPrivilegeEscalation != false (container "fedora" must set securityContext.allowPrivilegeEscalation=false), unrestricted capabilities (container "fedora" must set securityContext.capabilities.drop=["ALL"]), runAsNonRoot != true (pod or container "fedora" must set securityContext.runAsNonRoot=true), seccompProfile (pod or container "fedora" must set securityContext.seccompProfile.type to "RuntimeDefault" or "Localhost")
 
-cat <<EOF | oc -n test create -f -
+cat <<EOF | oc -n $ns create -f -
 ---
 kind: Pod
 apiVersion: v1
@@ -49,10 +50,10 @@ spec:
           - ALL
 EOF
 
-oc -n test get pod $pod_name -ojson|jq '.|{podname: .metadata.name, "openshift.io/scc": .metadata.annotations."openshift.io/scc"}'
-oc -n test get pod goodpod -ojson|jq '.|{podname: .metadata.name, "openshift.io/scc": .metadata.annotations."openshift.io/scc"}'
+oc -n $ns get pod $pod_name -ojson|jq '.|{podname: .metadata.name, "openshift.io/scc": .metadata.annotations."openshift.io/scc"}'
+oc -n $ns get pod goodpod -ojson|jq '.|{podname: .metadata.name, "openshift.io/scc": .metadata.annotations."openshift.io/scc"}'
 
 echo
-oc -n test adm policy scc-subject-review -u $(oc whoami) -f <(oc -n test get pod $pod_name -oyaml)
+oc -n $ns adm policy scc-subject-review -u $(oc whoami) -f <(oc -n $ns get pod $pod_name -oyaml)
 
 # oc describe scc anyuid
